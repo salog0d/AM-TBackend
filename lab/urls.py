19aa84bcccadb669c927b1e0d9d9e51bb4e76962
salog0d <david.salomon.nava11@gmail.com>
2025-04-02 10:28:33 -0600

    Added test urls

--- lab/urls.py
+++ lab/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path("createTest/", views.createTest, name="createTest"),
+    path("deleteTest/<int:test_id>/", views.deleteTest, name="deleteTest"),
+    path("updateTest/<int:test_id>/", views.updateTest, name="updateTest"),
+    path("viewTest/<int:test_id>/", views.viewTest, name="viewTest"),
 ]
